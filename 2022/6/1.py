4dfdcea14ce3df3834b9accae2c21eaf3c0709f5
Lennart Rambert Hammarström <lhammarstroem@gmail.com>
2022-12-06 16:19:52 +0100

    solve both 6.1 and 6.2 using the same abstraction

--- 2022/6/1.py
+++ 2022/6/1.py
@@ -2,16 +2,16 @@ with open('input.txt') as file:
     lines = [line.rstrip() for line in file]
 
 markers = []
+subset_length = 4
+
 for line in lines:
     characters = list(line)
 
     for i in range(0, len(characters)):
         if (i >= 3):
-            subset =\
-                characters[i-3] + \
-                characters[i-2] + \
-                characters[i-1] + \
-                characters[i]
+            subset = ''
+            for j in range(0, subset_length):
+                subset += characters[i-j]
 
             if (len(set(subset)) == len(subset)):
                 markers.append(i+1)
---
+++ 2022/6/2.py
@@ -0,0 +1,20 @@
+with open('input.txt') as file:
+    lines = [line.rstrip() for line in file]
+
+markers = []
+subset_length = 14
+
+for line in lines:
+    characters = list(line)
+
+    for i in range(0, len(characters)):
+        if (i >= 3):
+            subset = ''
+            for j in range(0, subset_length):
+                subset += characters[i-j]
+
+            if (len(set(subset)) == len(subset)):
+                markers.append(i+1)
+                break
+
+print(markers)
